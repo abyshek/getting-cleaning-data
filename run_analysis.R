3ce93811f7d293191555d75dedaa0c03ee52e510
Abyshek <abyshek.kumar@gmail.com>
2015-10-26 01:21:55 +0530

    some corrections in th R file

--- run_analysis.R
+++ run_analysis.R
@@ -45,15 +45,15 @@ run_analysis <- function()
   # Read subject_train text file to get the id of the test subjects 
   trainingsubject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
   
-  names(X_trainingdata) = data_colname
+  names(X_trainingdata) <- data_colname
   
   # Extract only the measurements on the mean and standard deviation for each measurement.
-  X_trainingdata = X_trainingdata[,measurement]
+  X_trainingdata <- X_trainingdata[,measurement]
   
   
-  Y_trainingdata[,2] = act_lbls[Y_trainingdata[,1]]
-  names(Y_trainingdata) = c("ACTIVITY_ID", "ACTIVITY_LABEL")  ## give meaningful labels
-  names(trainingsubject) = "PARTICIPANT"                      ## give meaningful labels
+  Y_trainingdata[,2] <- act_lbls[Y_trainingdata[,1]]
+  names(Y_trainingdata) <- c("ACTIVITY_ID", "ACTIVITY_LABEL")  ## give meaningful labels
+  names(trainingsubject) <- "PARTICIPANT"                      ## give meaningful labels
   
   ## merge the X and Y coordinates of training data
   trainingdata <- cbind(as.data.table(trainingsubject), Y_trainingdata, X_trainingdata)  
@@ -67,15 +67,15 @@ run_analysis <- function()
   Y_testdata <- read.table("./UCI HAR Dataset/test/y_test.txt")
   testsubject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
   
-  names(X_testdata) = data_colname
+  names(X_testdata) <- data_colname
   
   # Extract only the measurements
-  X_testdata = X_testdata[,measurement]
+  X_testdata <- X_testdata[,measurement]
   
   # Load activity labels
-  Y_testdata[,2] = act_lbls[Y_testdata[,1]]
-  names(Y_testdata) = c("ACTIVITY_ID", "ACTIVITY_LABEL")
-  names(testsubject) = "PARTICIPANT"
+  Y_testdata[,2] <- act_lbls[Y_testdata[,1]]
+  names(Y_testdata) <- c("ACTIVITY_ID", "ACTIVITY_LABEL")
+  names(testsubject) <- "PARTICIPANT"
   
   testdata <- cbind(as.data.table(testsubject), Y_testdata, X_testdata)
   
@@ -83,10 +83,10 @@ run_analysis <- function()
   
   ## Merge the worked out "training" and "test data" sets and create a tidy set with proper
   ## labels.
-  combinedata = rbind(testdata, trainingdata)
+  combinedata <- rbind(testdata, trainingdata)
   
-  id_label   = c("PARTICIPANT", "ACTIVITY_ID", "ACTIVITY_LABEL")
-  data_label = setdiff(colnames(combinedata), id_label)
+  id_label   <- c("PARTICIPANT", "ACTIVITY_ID", "ACTIVITY_LABEL")
+  data_label <- setdiff(colnames(combinedata), id_label)
   
   
   ##-----------------------Section 4--------------------------------------------------------
@@ -94,8 +94,8 @@ run_analysis <- function()
   
   ## Appending averages of the variables of each activity and each subject
   ## to create second tidy set
-  data_melt      = melt(combinedata, id = id_label, measure.vars = data_label)
-  final_data   = dcast(data_melt, PARTICIPANT + ACTIVITY_LABEL ~ variable, mean)
+  data_melt <- melt(combinedata, id = id_label, measure.vars = data_label)
+  final_data <- dcast(data_melt, PARTICIPANT + ACTIVITY_LABEL ~ variable, mean)
   ##------------------------------Section 5-------------------------------------------------
   
   ## Write the final tidy dataset to a text file named "tidy_data.txt"
